3e8e52e1d57e957feb5cd6e5ddba4ab9cb52df23
Krystian <milanello07@gmail.com>
2025-05-10 11:22:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import {useState} from "react";
 function App() {
     const [email, setEmail] = useState('abc@gmail.com');
 
+    let message;
+    if (email.length < 10) {
+        message = "Ale masz krotki adres!";
+    } else if (email.length < 15) {
+        message = "Adres jest okej!";
+    } else{
+        message = "Za dlugi adres!";
+}
+
+    function handleButtonclick(){
+        alert("Twoj adres email to: " + email);
+    }
+
     function handleChange(event){
       setEmail(event.target.value);
     }
+
   return (
-    <div className="App">
+    <div>
       <h1> system do zapisow zajec</h1>
       <h2> twoj email to: {email}</h2>
+      <div>{message} </div>
       <input type="text" onChange={handleChange} />
+        <button type = "button" onClick={handleButtonclick} > Wyswietl moj email w alercie</button>
     </div>
   );
-
 }
 export default App;
